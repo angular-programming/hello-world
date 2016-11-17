ea52f4002b8ab69890561e9298d20dfff0d51cf4
wubingjie <154266248@qq.com>
2016-11-17 20:40:57 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
+// main.ts
 import 'reflect-metadata';
 import 'zone.js';
 
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app.module';
-
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
